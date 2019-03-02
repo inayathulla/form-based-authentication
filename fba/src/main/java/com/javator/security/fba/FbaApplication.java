96c6c3367b1b32962b5fee4a379cc3171b9619f8
Inayathulla Khan <inayathulla2020@gmail.com>
2019-03-02 09:03:10 +0530

    First commit

---
+++ fba/src/main/java/com/javator/security/fba/FbaApplication.java
@@ -0,0 +1,13 @@
+package com.javator.security.fba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbaApplication.class, args);
+	}
+
+}
---
+++ fba/src/test/java/com/javator/security/fba/FbaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javator.security.fba;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FbaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
